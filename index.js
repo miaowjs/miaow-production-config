18a1439cef9546f551854ae5c026408353cc9714
梁枫 <liangfeng@corp.netease.com>
2015-08-20 16:22:15 +0800

    增加内容嵌入的留白

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var cssUrlParse = {
 var inlineContentParse = {
 	plugin: inlineParse,
 	option: {
-		regexp: /((?:\/\*|<!--)\s*inline\s+['"]([\w\_\/\.\-]+)['"](?:\*\/|-->))/g,
+		regexp: /((?:\/\*|<!--)\s*inline\s+['"]([\w\_\/\.\-]+)['"]\s*(?:\*\/|-->))/g,
 		type: 'content'
 	}
 };
